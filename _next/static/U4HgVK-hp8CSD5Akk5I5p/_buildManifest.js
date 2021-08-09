8be6851f66890096b5f9bf5e48c3ce4566c4a13f
kasatomo <kasatomo@users.noreply.github.com>
2021-08-09 00:39:36 +0000

    deploy: 3a82c298201c81247d37adac8567fb1c830cca5d

---
+++ _next/static/U4HgVK-hp8CSD5Akk5I5p/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d0196bdddeb2157bb113.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
